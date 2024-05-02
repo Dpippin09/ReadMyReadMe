c0fd709a626f11d67f3643f3c2af3d0e92079382
unknown <dpippin09@gmail.com>
2024-05-02 13:12:59 -0600

    updated work

--- index.js
+++ index.js
@@ -1,10 +1,61 @@
 // TODO: Include packages needed for this application
-
+const { renderLiscenseBadge, renderLiscenseLink, renderLiscenseSection}
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        messgae: "What is the name of your project?",
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install you project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do we use your project',
+    },
+    {
+        type: 'list',
+        name: 'liscense',
+        message: 'Please choose a liscense',
+        choices: ['MIT', 'Apache2.0', 'none']
+    },
+    {
+        type: 'input',
+        name: 'contrinuting',
+        message: 'Please enter the contribution guidlines',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How to test your project',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Please enter you contact information for the questions'
+    },
+
+];
+  
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    FileSystem.writeFileSync(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Successfully wrote to" + fileName);
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
